acc471c2a17a64944150abe7dd93f8415e7b9134
BreakingFuse <BreakingFuse@hotmail.com>
2021-02-22 15:14:31 -0600

    Re-add Movement Activity features

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public enum MoveState {Moving, Waiting}
+
     // Initialize Variables
-    public Vector3 target;
+    public Vector3 goal;
+    private MoveState myMoveState = MoveState.Waiting;
     float speed = 2f;
     float accuracy = .5f;
     float rotSpeed = 2f;
@@ -14,7 +17,8 @@ public class Move : MonoBehaviour
     void Start()
     {
         //initialize target (this transform's position upon initialization)
-        target = this.transform.position;
+        goal = this.transform.position;
+
     }
 
     // Update is called once per frame
@@ -27,15 +31,15 @@ public class Move : MonoBehaviour
         //Perform raycast with left-click and use hit to determine new target position
         if (Physics.Raycast(ray, out hit) && Input.GetMouseButton(0))
         {
-            target = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
-            
+            goal = new Vector3(hit.point.x, this.transform.position.y, hit.point.z);
+            myMoveState = MoveState.Moving;
         }
 
         //Direction of this transform
-        Vector3 direction = target - transform.position;
+        Vector3 direction = goal - transform.position;
 
         //if Distance too high
-        if (Vector3.Distance(transform.position, target) > accuracy)
+        if (Vector3.Distance(transform.position, goal) > accuracy)
         {
             this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
                                                        Quaternion.LookRotation(direction), 
@@ -43,6 +47,33 @@ public class Move : MonoBehaviour
             this.transform.Translate(0, 0, speed * Time.deltaTime);
         }
 
+        //Made it to the goal
+        if (Vector3.Distance(transform.position, goal) <= accuracy && myMoveState == MoveState.Moving)
+        {
+            Debug.Log($"{this} has made it to their goal!");
+            
+            StartCoroutine("Dance");
+
+            myMoveState = MoveState.Waiting;
+        }
+
+    }
+
+    IEnumerator Dance()
+    {
+        Debug.Log("Dancing");
+        goal = new Vector3(this.transform.position.x,
+                               this.transform.position.y + 5,
+                               this.transform.position.z);
+        this.transform.localScale *= 1.50f;
+        this.transform.Rotate(0, 0, 360);
+        yield return new WaitForSeconds(2f);
+        this.transform.localScale /= 1.50f;
+        goal = new Vector3(this.transform.position.x + Random.Range(-10, 10), 
+                            0.5f, 
+                           this.transform.position.z + Random.Range(-10, 10));
+        this.transform.rotation = Quaternion.Euler(0,0,0);
+        yield return null;
     }
 
 }
